fea3695453b935c078e45f473ba2c81524787d56
syafrudinhl <syafrudintdm@gmail.com>
2023-11-13 16:24:19 +0700

    edit : layout Router

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,25 @@
-import Sidebar from './Sidebar';
-import Topbar from './Topbar';
+import React from "react";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import Routers from "../../router/Routers";
+import { Container, Row, Col } from "react-bootstrap";
 
-export default function Layout(props) {
-    return (
-        <div>
-            <Topbar />
+export default function Layout() {
+  return (
+    <Container fluid>
+      <Row as="row">
+        <Col lg="12">
+          <Topbar />
+        </Col>
+        <Row as="row">
+          <Col lg="2">
             <Sidebar />
-        </div>
-    );
+          </Col>
+          <Col lg="10">
+            <Routers />
+          </Col>
+        </Row>
+      </Row>
+    </Container>
+  );
 }
--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Complaint from "../pages/Complaint";
 
 export default function Routers() {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard"></Navigate>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="/complaint" element={<Complaint></Complaint>}></Route>
+    </Routes>
+  );
 }
