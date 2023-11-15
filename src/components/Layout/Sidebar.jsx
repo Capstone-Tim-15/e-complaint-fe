12c9e54658f7d910ba478e93b24e3bc3fceb9351
rafidardanu <rescuephones2016@gmail.com>
2023-11-16 04:28:35 +0700

    dev sidebar

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -9,11 +9,10 @@ import { HiOutlineLogout } from "react-icons/hi";
 const Sidebar = () => {
   const navigate = useNavigate();
   return (
-    <div className="sidebar all-sidebar d-flex flex-column justify-content-center align-items-center text-white vh-100">
+    <div className="sidebar all-sidebar d-none d-sm-flex flex-column justify-content-start align-items-center text-white">
       <div className="text-center">
-        <hr className="text-secondary" />
         <ul className="nav flex-column p-0 m-0">
-          <li className="nav-item p-1">
+          <li className="nav-item p-4 mt-4">
             <Link
               to="/dashboard"
               className="nav-link text-white text-decoration-none d-flex align-items-center"
@@ -22,7 +21,7 @@ const Sidebar = () => {
               <span className="fs-5">Dashboard</span>
             </Link>
           </li>
-          <li className="nav-item p-1">
+          <li className="nav-item p-4">
             <Link
               to="/complaint"
               className="nav-link text-white text-decoration-none d-flex align-items-center"
@@ -31,7 +30,7 @@ const Sidebar = () => {
               <span className="fs-5">Complaint</span>
             </Link>
           </li>
-          <li className="nav-item p-1">
+          <li className="nav-item p-4">
             <Link
               to="/berita"
               className="nav-link text-white text-decoration-none d-flex align-items-center"
@@ -40,7 +39,7 @@ const Sidebar = () => {
               <span className="fs-5">Berita</span>
             </Link>
           </li>
-          <li className="nav-item p-1">
+          <li className="nav-item p-4">
             <Link
               to="/chat"
               className="nav-link text-white text-decoration-none d-flex align-items-center"
@@ -49,7 +48,7 @@ const Sidebar = () => {
               <span className="fs-5">Chat</span>
             </Link>
           </li>
-          <li className="nav-item p-1">
+          <li className="nav-item p-4">
             <Link
               to="/export"
               className="nav-link text-white text-decoration-none d-flex align-items-center"
@@ -61,8 +60,7 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="text-center mt-auto">
-        <hr className="text-secondary" />
+      <div className="text-center button-side mt-5">
         <Button
           onClick={() => navigate("/")}
           variant="primary"
