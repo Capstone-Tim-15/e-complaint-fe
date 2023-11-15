6c44435b912e10496a99abd31a67f9f7c19971ef
syafrudinhl <syafrudintdm@gmail.com>
2023-11-15 23:30:26 +0700

    edit : add Sidebar & Topbar components

--- src/pages/ChatList.jsx
+++ src/pages/ChatList.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Card, Button, Col, Row } from "react-bootstrap";
+import Sidebar from "../components/Layout/Sidebar";
+import Topbar from "../components/Layout/Topbar";
 
 const ChatList = () => {
   const [chatList, setChatList] = useState([]);
   const [showChatbot, setShowChatbot] = useState(false);
 
   useEffect(() => {
-    fetch('https://655422dd63cafc694fe62bc5.mockapi.io/listchat/chatlist')
-      .then(response => response.json())
-      .then(data => setChatList(data.slice(0, 7)))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("https://655422dd63cafc694fe62bc5.mockapi.io/listchat/chatlist")
+      .then((response) => response.json())
+      .then((data) => setChatList(data.slice(0, 7)))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   const toggleChatbot = () => {
@@ -17,32 +19,47 @@ const ChatList = () => {
   };
 
   const styleButton = {
-    position: 'fixed',
+    position: "fixed",
     bottom: 10,
     right: 10,
-    borderRadius: '50%',
-    width: '80px', 
-    height: '80px', 
-    fontSize: '14px',
-    backgroundColor: 'gray',
+    borderRadius: "50%",
+    width: "80px",
+    height: "80px",
+    fontSize: "14px",
+    backgroundColor: "gray",
   };
 
   return (
-    <div>
-      <h1>chat list enduser</h1>
-      
-      {chatList.map(chat => (
-        <Card key={chat.id} style={{ width: '18rem', marginBottom: '10px' }}>
-          <Card.Body>
-            <Card.Title>{chat.sender}</Card.Title>
-          </Card.Body>
-        </Card>
-      ))}
-        <Button style={{ ...styleButton }} onClick={toggleChatbot}>
-            tanya
-        </Button>
-
-    </div>
+    <>
+      <Row as="row">
+        <Col lg="12">
+          <Topbar />
+        </Col>
+        <Row as="row">
+          <Col lg="2">
+            <Sidebar />
+          </Col>
+          <Col lg="10">
+            <div>
+              <h1>chat list enduser</h1>
+              {chatList.map((chat) => (
+                <Card
+                  key={chat.id}
+                  style={{ width: "18rem", marginBottom: "10px" }}
+                >
+                  <Card.Body>
+                    <Card.Title>{chat.sender}</Card.Title>
+                  </Card.Body>
+                </Card>
+              ))}
+              <Button style={{ ...styleButton }} onClick={toggleChatbot}>
+                tanya
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Row>
+    </>
   );
 };
 
