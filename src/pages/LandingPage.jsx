c0031bd4ecce2f6292836a1c4497be535fa7c16e
syafrudinhl <syafrudintdm@gmail.com>
2023-11-15 12:13:37 +0700

    edit : import Capabilities Components

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,9 +6,9 @@ import Footer from "../components/LandingPage/Footer/Footer";
 import image1 from "../assets/lp-1.png";
 import image2 from "../assets/lp-2.png";
 import image3 from "../assets/lp-3.png";
-import image4 from "../assets/lp-4.png";
 import Blog from "../components/BlogPost/blog";
 import GuideList from "../components/guide/GuideList";
+import Capabilites from "../components/Capabilities/Capabilites";
 import Layout from "../components/Layout/Layout";
 import "../styles/landing-page.css";
 
@@ -85,47 +85,7 @@ export default function LandingPage() {
       </section>
 
       <section className="section__4">
-        <Container>
-          <div className="sub__section-4">
-            <Col
-              lg="9"
-              className="cap__col d-flex justify-content-center align-items-center"
-            >
-              <div className="title__section-4">
-                <p>Our Capabilities</p>
-                <h1>Make yourself heard,</h1>
-                {""}
-                <h1>voice your complaints</h1>
-                <span className="subtitle">
-                  mari kita bersama-sama menyuarakan{" "}
-                </span>
-                <br />
-                <span className="subtitle">
-                  keluhan dan aspirasi kita untuk menciptakan{" "}
-                </span>
-                <br />
-                <span className="subtitle">perubahan yang lebih baik</span>
-                <div className="subtitle__section-4 d-flex gap-5 mt-5">
-                  <div>
-                    <div className="cap__title">575</div>
-                    <div className="cap__subtitle">Total User</div>
-                  </div>
-                  <div>
-                    <div className="cap__title">1.500</div>
-                    <div className="cap__subtitle">Total Complaint</div>
-                  </div>
-                  <div>
-                    <div className="cap__title">2.000</div>
-                    <div className="cap__subtitle">Resolved Complaint </div>
-                  </div>
-                </div>
-              </div>
-            </Col>
-            <Col lg="3">
-              <img className="lp__4" src={image4} alt="" />
-            </Col>
-          </div>
-        </Container>
+        <Capabilites></Capabilites>
       </section>
 
       <section>
