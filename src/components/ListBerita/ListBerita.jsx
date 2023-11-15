1c077d3fe1f3b8926a5bf4ec9b34333e2fc98e3a
dianputri1 <dp934786@gmail.com>
2023-11-15 17:08:16 +0700

    List Berita Revisi

--- src/components/ListBerita/ListBerita.jsx
+++ src/components/ListBerita/ListBerita.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Icon } from "@iconify/react";
-import './ListBerita.css';
+import './ListBerita.css'
 
 const ListBerita = () => {
+  const [news, setNews] = useState ([]);
 
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    const response = await axios.get("https://6554737263cafc694fe67aef.mockapi.io/berita");
+    setNews(response.data);
+  };
+
+  const addNews = async () => {
+    try {
+      await axios.post(`https://6554737263cafc694fe67aef.mockapi.io/berita`, newNews);
+      fetchBooks();
+      setNewBook({ judul: '', author: '', tanggal:'', status:'' });
+    } catch (error) {
+      console.error('Error adding news: ', error);
+    }
+  };
+
+  const deleteNews = async (id) => {
+    try {
+      await axios.delete(`https://6554737263cafc694fe67aef.mockapi.io`);
+      getNews();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
     return(
       <div>
@@ -23,22 +52,24 @@ const ListBerita = () => {
         </tr>
       </thead>
       <tbody>
-          <tr>
-          <th scope="row">Fery Admin</th>
-          <td >Investasi Prospektif, Ini Alasannya</td>
-          <td >31 Oktober 2023</td>
+        {news.map((news, index) => (
+          <tr key={news.id}>
+          <td>{news.author}</td>
+          <td >{news.judul}</td>
+          <td >{news.tanggal}</td>
           <td className="status">
-          <p className="bg-warning text-white" id="text2" >Proses</p>
+          <p className="bg-warning text-white" id="text2" >{news.status}</p>
           </td>
           <td className="button me-1">
           <button className="me-2" id="btn">
                   <Icon icon="uil:edit" width="35" height="35"/>
                 </button>
-          <button id="btn">
+          <button  onClick={() => deleteNews(news.id)} id="btn">
                   <Icon icon="mdi:trash-can-outline" width="35" height="35" />
             </button>
           </td>
         </tr>
+        ))}
       </tbody>
     </table>
       </div>
