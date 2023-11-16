a45356367c3a52bb18b5635e6253f6217f7b0cfd
rafidardanu <rescuephones2016@gmail.com>
2023-11-17 01:10:16 +0700

    add data dummy notif

---
+++ src/components/Notification/DummyNotif.jsx
@@ -0,0 +1,32 @@
+export const dataNotif = [
+  {
+    nama: "Jelita Salsabila",
+    keluhan: "Selokan di jalan Diponegoro kotor nich! Ini sudah beberapa hari sejak saya melapor, tapi belum ada tindakan. Tolong segera bersihkan karena sudah sangat mengganggu warga sekitar.",
+    kategori: "Layanan",
+    waktu: "5 menit yang lalu",
+  },
+  {
+    nama: "Gunawan Santoso",
+    keluhan: "Sekolah SMAN 1 lagi ada pembulian min! Saya sudah berkali-kali melaporkan masalah ini ke pihak sekolah, tapi belum ada tindakan konkret. Mohon segera diatasi agar tidak merugikan siswa-siswa yang menjadi korban.",
+    kategori: "Bullying",
+    waktu: "2 jam yang lalu",
+  },
+  {
+    nama: "Firman Maulana",
+    keluhan: "Jalan-jalan di sekitar rumah ada sampah min! Setiap hari saya melihat banyak sampah berserakan di sekitar lingkungan rumah. Saya berharap pihak terkait dapat melakukan pembersihan secara rutin untuk menjaga kebersihan lingkungan.",
+    kategori: "Layanan",
+    waktu: "3 jam yang lalu",
+  },
+  {
+    nama: "Citra Dewi",
+    keluhan: "Pelayanan kesehatan di puskesmas kurang memadai. Saya merasa pelayanan di puskesmas setempat tidak sesuai dengan harapan. Antrian panjang, tenaga medis kurang, dan obat-obat seringkali kosong. Tolong perhatikan hal ini agar masyarakat dapat mendapatkan pelayanan kesehatan yang baik.",
+    kategori: "Pelayanan Kesehatan",
+    waktu: "5 jam yang lalu",
+  },
+  {
+    nama: "Budi Prasetyo",
+    keluhan: "Taman kota seringkali tidak terawat dengan baik. Sebagai warga, saya berharap taman kota dapat dijaga kebersihannya. Saat ini banyak sampah berserakan dan fasilitas taman rusak. Mohon perhatian lebih untuk menjaga keindahan taman kota ini.",
+    kategori: "Lingkungan",
+    waktu: "1 hari yang lalu",
+  },
+];
