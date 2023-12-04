200dc1036ce2c0b277e429bd5c3f0327519d9355
Fauzan <fabdillah408@gmail.com>
2023-12-04 11:27:45 +0700

    global auth token use contexts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,10 +8,12 @@ import Vector from "../assets/vector.png";
 import Ellipse from "../assets/ellipse.png";
 import "../styles/login.css";
 import axios from "axios";
+import { useAuth } from "../contexts/authContext";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+  const { setToken } = useAuth();
 
   const userRef = useRef();
   const errRef = useRef();
@@ -40,7 +42,8 @@ export default function Login() {
       .then((result) => {
         console.log(result);
         const tokenBarear = result.data.results.token;
-        localStorage.setItem("token", tokenBarear);
+        // localStorage.setItem("token", tokenBarear);
+        setToken(tokenBarear);
         console.log(tokenBarear);
         navigate("/dashboard");
       })
@@ -72,47 +75,15 @@ export default function Login() {
             <div className="content">
               <div className="title__login">Gov - Complaint</div>
               <div className="sub__title mb-4">Admin Panel Login</div>
-              <p
-                ref={errRef}
-                className={errMsg ? "errmsg" : "offscreen"}
-                aria-live="assertive"
-              >
+              <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
                 {errMsg}
               </p>
-              <form
-                onSubmit={handleSubmit}
-                className="d-flex flex-column align-item-center"
-              >
-                <Form.Control
-                  className="email__input mb-4"
-                  type="text"
-                  id="username"
-                  placeholder="Username"
-                  ref={userRef}
-                  autoComplete="off"
-                  onChange={(e) => setUsername(e.target.value)}
-                  value={username}
-                  required
-                />
+              <form onSubmit={handleSubmit} className="d-flex flex-column align-item-center">
+                <Form.Control className="email__input mb-4" type="text" id="username" placeholder="Username" ref={userRef} autoComplete="off" onChange={(e) => setUsername(e.target.value)} value={username} required />
                 <InputGroup className="input__group-pass mb-3">
-                  <Form.Control
-                    className="password__input"
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Kata Sandi"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                  />
-                  <Button
-                    as="button"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? (
-                      <Icon icon="mdi:eye-off" />
-                    ) : (
-                      <Icon icon="mdi:eye" />
-                    )}
+                  <Form.Control className="password__input" id="password" type={showPassword ? "text" : "password"} placeholder="Kata Sandi" onChange={(e) => setPassword(e.target.value)} value={password} required />
+                  <Button as="button" onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <Icon icon="mdi:eye-off" /> : <Icon icon="mdi:eye" />}
                   </Button>
                 </InputGroup>
                 <Button type="submit" className="login__button mb-4">
