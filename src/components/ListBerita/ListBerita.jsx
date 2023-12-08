c89e7308769f01075f4718b71ec04804e968a74f
dianputri1 <dp934786@gmail.com>
2023-12-08 13:40:01 +0700

    Revisi list berita

--- src/components/ListBerita/ListBerita.jsx
+++ src/components/ListBerita/ListBerita.jsx
@@ -22,7 +22,7 @@ const ListBerita = () => {
   const confirmDelete = async () => {
     try {
           const token = localStorage.getItem("token"); // Ambil token dari localStorage
-          await axios.delete(`http://34.128.69.15:8000/admin/news/${newsId}`, {
+          await axios.delete(`https://api.govcomplain.my.id/admin/news/${newsId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -38,7 +38,7 @@ const ListBerita = () => {
   const getNews = async () => {
     try {
       const token = localStorage.getItem("token"); // Ambil token dari localStorage
-      const response = await axios.get("http://34.128.69.15:8000/admin/news?page=" + currentPage, {
+      const response = await axios.get("https://api.govcomplain.my.id/admin/news?page=" + currentPage, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -54,20 +54,6 @@ const ListBerita = () => {
   const deleteNews = async (newsId) => {
     setModal(true)
     setNewsId(newsId);
-    // if (window.confirm("Are you sure you want to delete this news?")) {
-    //   try {
-    //     const token = localStorage.getItem("token"); // Ambil token dari localStorage
-    //     await axios.delete(`http://34.128.69.15:8000/admin/news/${newsId}`, {
-    //       headers: {
-    //         Authorization: `Bearer ${token}`,
-    //       },
-    //     });
-    //     getNews();
-    //   } catch (error) {
-    //     console.error("Error deleting this news request ", error);
-    //     setError("Error deleting news. Please check your JWT token.");
-    //   }
-    // }
   };
 
   function Pagination({ meta }) {
@@ -110,14 +96,14 @@ const ListBerita = () => {
       </Row>
       <div className="mt-2">
         {error && <p style={{ color: "red" }}>{error}</p>}
-        <table id="table__berita" className="table table-borderless text-left">
+        <table id="table__berita" className="table table-bordered text-left">
           <thead id="table__berita" className="thead">
             <tr>
               <th scope="col">Author</th>
               <th scope="col">Judul</th>
               <th scope="col">Konten</th>
               <th scope="col">Tanggal</th>
-              {/* <th scope="col">Status</th> */}
+               <th scope="col">Status</th>
               <th scope="col"> </th>
             </tr>
           </thead>
@@ -128,11 +114,11 @@ const ListBerita = () => {
                 <td>{item.title}</td>
                 <td>{item.content}</td>
                 <td>{item.date}</td>
-                {/* <td className="status">
+                <td className="status">
                   <p className="bg-warning text-white" id="text2">
                     {item.status}
                   </p>
-                </td> */}
+                </td>
                 <td className="button me-1">
                   <div className="d-flex">
                     <button onClick={() => {navigate(`/editberita?id=${item.id}`)}} className="me-2" id="btn">
